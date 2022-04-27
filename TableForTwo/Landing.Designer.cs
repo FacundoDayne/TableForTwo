df04a423e94e5129c47fe06c9a38e5ac5e74bd2b
FacundoDayne <facundo.088947@globalcity.sti.edu.ph>
2022-04-27 11:12:27 +0800

    Added Tables and Sign up controls

--- TableForTwo/Landing.Designer.cs
+++ TableForTwo/Landing.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.login1 = new TableForTwo.Login();
+            this.signup1 = new TableForTwo.Signup();
+            this.tables1 = new TableForTwo.Tables();
             this.SuspendLayout();
             // 
             // login1
@@ -39,6 +41,24 @@
             this.login1.Size = new System.Drawing.Size(1286, 752);
             this.login1.TabIndex = 0;
             // 
+            // signup1
+            // 
+            this.signup1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.signup1.Location = new System.Drawing.Point(-3, 1);
+            this.signup1.Name = "signup1";
+            this.signup1.Size = new System.Drawing.Size(1290, 751);
+            this.signup1.TabIndex = 1;
+            this.signup1.Visible = false;
+            // 
+            // tables1
+            // 
+            this.tables1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tables1.Location = new System.Drawing.Point(0, 0);
+            this.tables1.Name = "tables1";
+            this.tables1.Size = new System.Drawing.Size(1290, 751);
+            this.tables1.TabIndex = 2;
+            this.tables1.Visible = false;
+            // 
             // Landing
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -46,6 +66,8 @@
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1274, 745);
             this.Controls.Add(this.login1);
+            this.Controls.Add(this.signup1);
+            this.Controls.Add(this.tables1);
             this.MaximumSize = new System.Drawing.Size(1290, 784);
             this.MinimumSize = new System.Drawing.Size(1290, 784);
             this.Name = "Landing";
@@ -57,5 +79,7 @@
         #endregion
 
         private Login login1;
+        private Signup signup1;
+        private Tables tables1;
     }
 }
