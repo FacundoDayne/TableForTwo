261ad542b0bb9fc8c4b65bfc72a59da3f6f0f601
= <=>
2022-05-07 05:35:33 +0800

    Refactored infos to UserInformation then added constructor

---
+++ TableForTwo/UserInformation.cs
@@ -0,0 +1,10 @@
+﻿class UserInformation
+{
+    public string UserEmail, UserPassword;
+
+    public UserInformation(string UserEmail, string UserPassword)
+    {
+        this.UserEmail = UserEmail;
+        this.UserPassword = UserPassword;
+    }
+}
