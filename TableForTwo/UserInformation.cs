88bdb5295e2aee351685377543526e0563996b8c
= <=>
2022-05-08 21:08:34 +0800

    Added UserInformation functionalities and static attribute

--- TableForTwo/UserInformation.cs
+++ TableForTwo/UserInformation.cs
@@ -1,10 +1,22 @@
 ﻿class UserInformation
 {
+    private static UserInformation? LoggedInUser;
+
     public string UserEmail, UserPassword;
 
     public UserInformation(string UserEmail, string UserPassword)
     {
         this.UserEmail = UserEmail;
         this.UserPassword = UserPassword;
+    }
+
+    public static UserInformation? GetLoggedInUser()
+    {
+        return LoggedInUser;
+    }
+
+    public static void SetLoggedInUser(UserInformation user)
+    {
+        LoggedInUser = user;
     }
 }
